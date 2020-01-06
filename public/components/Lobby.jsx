f4139179396f37111d62831df72d508fc1516169
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-01-06 15:23:38 -0500

    Create Lobby.jsx

---
+++ public/components/Lobby.jsx
@@ -0,0 +1,36 @@
+var roomSummary = function (room, roomName) {
+  var cx = React.addons.classSet
+  var classes = cx({
+    "list-group-item": true,
+    "active": room.name === roomName
+  });
+  return (
+  <a className={classes} href={"#room/" + room.name}>
+    <span className="badge">{room.users.length}</span>
+    {room.name || "app"}
+  </a>
+  );
+};
+
+var Lobby = React.createClass({
+  
+  render: function () {
+    var rooms = this.props.rooms
+      , roomName = this.props.roomName
+      , self = this;
+
+    return (
+    <div className="col-md-3">
+      <div className="list-group">
+      {
+        rooms.map(function (room) {
+          return roomSummary(room, roomName);
+        })
+      }
+      </div> 
+    </div>
+    ); 
+  }
+});
+
+module.exports = Lobby;
