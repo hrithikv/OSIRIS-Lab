8e4acd0607fae3eac6528a8e021c8ccbaf61c78f
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-01-06 19:14:30 -0500

    Update app-server.js

--- app-server.js
+++ app-server.js
@@ -25,7 +25,7 @@ additionRoom.start();
 subtractionRoom.start();
 multiplicationRoom.start();
 
-var handleBegin = _.curry(function (socket, roomManager, data) {
+var selectBegin = _.curry(function (socket, roomManager, data) {
   var id = data.id
     , user = new AppWizard(socket, data.name || "AppWizard");
 
@@ -34,7 +34,7 @@ var handleBegin = _.curry(function (socket, roomManager, data) {
   socket.emit("begin-confirm", user.serializeState());
 });
 
-var handleDisconnect = _.curry(function (socket, roomManager, data) {
+var selectDisconnect = _.curry(function (socket, roomManager, data) {
   var user = roomManager.socketToUserMap[socket.id];
 
   if (user) {
@@ -43,7 +43,7 @@ var handleDisconnect = _.curry(function (socket, roomManager, data) {
   }
 });
 
-var handleSubmission = _.curry(function (socket, roomManager, answer) {
+var selectSubmission = _.curry(function (socket, roomManager, answer) {
   var user = roomManager.socketToUserMap[socket.id]; 
 
   user.room.storeSubmission({
@@ -52,7 +52,7 @@ var handleSubmission = _.curry(function (socket, roomManager, answer) {
   });
 });
 
-var handleJoin = _.curry(function (socket, roomManager, roomName) {
+var selectJoin = _.curry(function (socket, roomManager, roomName) {
   var user = roomManager.socketToUserMap[socket.id]
     , targetRoom = roomManager.getRoomByName(roomName)
     , lobby = roomManager.getLobby();
@@ -68,7 +68,7 @@ var handleJoin = _.curry(function (socket, roomManager, roomName) {
   }
 });
 
-var handleNameChange = _.curry(function (socket, roomManager, name) {
+var selectNameChange = _.curry(function (socket, roomManager, name) {
   var user = roomManager.socketToUserMap[socket.id];
 
   user.name = name;
@@ -77,11 +77,11 @@ var handleNameChange = _.curry(function (socket, roomManager, name) {
 
 server.sockets.on("connection", function (socket) {
   socket 
-    .on("begin", handleBegin(socket, roomManager))
-    .on("disconnect", handleDisconnect(socket, roomManager))
-    .on("submission", handleSubmission(socket, roomManager))
-    .on("name-change", handleNameChange(socket, roomManager))
-    .on("join", handleJoin(socket, roomManager))
+    .on("begin", selectBegin(socket, roomManager))
+    .on("disconnect", selectDisconnect(socket, roomManager))
+    .on("submission", selectSubmission(socket, roomManager))
+    .on("name-change", selectNameChange(socket, roomManager))
+    .on("join", selectJoin(socket, roomManager))
 });
 
 app.listen(8080, console.log.bind(console, "server on 8080"));
